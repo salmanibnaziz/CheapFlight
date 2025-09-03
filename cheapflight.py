e9efd33fea923c90a722affec5a31e5eee08b2f0
salmanibnaziz <salmanibnaziz@gmail.com>
2025-09-02 20:21:08 -0400

    Update cheapflight.py

--- cheapflight.py
+++ cheapflight.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1Kv6FaOMVRwISjF_eyubUt6ZUzS4bHp3l
 """
 
-!pip install streamlit
+pip install streamlit
 import streamlit as st
 import pandas as pd
 
